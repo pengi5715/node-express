9f8c819035dbba531db92b5beab6ba18100d7ef1
pengi5715 <139732822+pengi5715@users.noreply.github.com>
2023-07-17 15:56:20 +0900

    Create imageRouter.js

---
+++ routes/imageRouter.js
@@ -0,0 +1,31 @@
+// imageRouter.js
+import { Router } from "express";
+import axios from "axios";
+
+var client_id = 'j8PIiTpiuPi6PzcQD4MV';  //Client ID
+var client_secret = 'DuZ9KkT_ZH';  //Client Secret
+
+export const imageRouter = Router();
+
+function imageSearch(req, res, next) {
+  const api_url =
+    "https://openapi.naver.com/v1/search/book.json?query=" +
+    encodeURI(req.query.query);
+  axios
+    .get(api_url, {
+      headers: {
+        "X-Naver-Client-Id": client_id,
+        "X-Naver-Client-Secret": client_secret,
+      },
+    })
+    .then((data) => {
+      res.send(data.data.items);
+    })
+    .catch((err) => next(err));
+}
+
+imageRouter.get("https://port-0-node-express-fhkg2nlk6gtfkp.sel4.cloudtype.app/", imageSearch);
+
+
+
+
