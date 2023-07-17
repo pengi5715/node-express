25cb89d6e02d920c3ee616a66581c88b0e8e9e45
pengi5715 <139732822+pengi5715@users.noreply.github.com>
2023-07-17 16:27:45 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
+import { Router } from 'express'
+import axios from 'axios'
+
+const client_id = 'j8PIiTpiuPi6PzcQD4MV'
+const client_secret = 'DuZ9KkT_ZH'
+
+export const router = Router()
+
+function searchBooks(req, res) {
+  const query = req.query.query
+  const api_url = `https://openapi.naver.com/v1/search/book.json?query=${encodeURIComponent(query)}`
+  axios.get(api_url, {
+    headers: {
+      'X-Naver-Client-Id': client_id,
+      'X-Naver-Client-Secret': client_secret
+    }
+  })
+  .then(response => {
+    res.send(response.data.items)
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).send('Internal Server Error')
+  })
+}
+
+router.get('/books', searchBooks)
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.html');
+});
+
+
+
+/*
 var express = require('express');
 var router = express.Router();
 
-/* 주석임 GET home page. */
+/* 주석임 GET home page.
 router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
 module.exports = router;
+*/
